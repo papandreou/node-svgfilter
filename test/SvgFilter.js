d0092428c8c0b1456b52892c8c8961fd887f8a28
Andreas Lind <andreas@one.com>
2015-05-04 21:44:02 +0200

    Test: Use unexpected-stream.

--- test/SvgFilter.js
+++ test/SvgFilter.js
@@ -1,84 +1,60 @@
 /* global describe, it */
-var expect = require('unexpected'),
+var unexpected = require('unexpected'),
     SvgFilter = require('../lib/SvgFilter'),
     Path = require('path'),
     fs = require('fs');
 
 describe('SvgFilter', function () {
-    it('should produce a smaller file when exporting only a specific ID', function (done) {
-        var svgFilter = new SvgFilter({keepId: ['linearGradient3175']}),
-            chunks = [];
-        fs.createReadStream(Path.resolve(__dirname, 'data', 'dialog-information.svg'))
-            .pipe(svgFilter)
-            .on('data', function (chunk) {
-                chunks.push(chunk);
-            })
-            .on('end', function () {
-                var resultSvgBuffer = Buffer.concat(chunks);
-                expect(resultSvgBuffer.length, 'to be greater than', 0);
-                expect(resultSvgBuffer.length, 'to be less than', 38289);
-                var resultSvgText = resultSvgBuffer.toString('utf-8');
-                expect(resultSvgText, 'to match', /id="linearGradient3175"/);
-                expect(resultSvgText, 'not to match', /id="linearGradient2399"/);
-                done();
-            })
-            .on('error', done);
+    var expect = unexpected.clone().installPlugin(require('unexpected-stream'));
+
+    it('should produce a smaller file when exporting only a specific ID', function () {
+        return expect(
+            fs.createReadStream(Path.resolve(__dirname, 'data', 'dialog-information.svg')),
+            'when piped through',
+            new SvgFilter({keepId: ['linearGradient3175']}),
+            'to yield output satisfying',
+            'when decoded as', 'utf-8',
+            expect.it('to contain', 'id="linearGradient3175"')
+                .and('not to contain', 'id="linearGradient2399"')
+        );
     });
 
-    it('should produce a smaller file when exporting only a specific ID, command-line argument style', function (done) {
-        var svgFilter = new SvgFilter(['--keepId=linearGradient3175']),
-            chunks = [];
-        fs.createReadStream(Path.resolve(__dirname, 'data', 'dialog-information.svg'))
-            .pipe(svgFilter)
-            .on('data', function (chunk) {
-                chunks.push(chunk);
-            })
-            .on('end', function () {
-                var resultSvgBuffer = Buffer.concat(chunks);
-                expect(resultSvgBuffer.length, 'to be greater than', 0);
-                expect(resultSvgBuffer.length, 'to be less than', 38292);
-                var resultSvgText = resultSvgBuffer.toString('utf-8');
-                expect(resultSvgText, 'to match', /id="linearGradient3175"/);
-                expect(resultSvgText, 'not to match', /id="linearGradient2399"/);
-                done();
-            })
-            .on('error', done);
+    it('should produce a smaller file when exporting only a specific ID, command-line argument style', function () {
+        return expect(
+            fs.createReadStream(Path.resolve(__dirname, 'data', 'dialog-information.svg')),
+            'when piped through',
+            new SvgFilter(['--keepId=linearGradient3175']),
+            'to yield output satisfying',
+            'when decoded as', 'utf-8',
+            expect.it('to contain', 'id="linearGradient3175"')
+                .and('not to contain', 'id="linearGradient2399"')
+        );
     });
 
-    it('should execute inline JavaScript with the specified id', function (done) {
-        var svgFilter = new SvgFilter({runScript: 'run', injectId: 'theId'}),
-            chunks = [];
-        fs.createReadStream(Path.resolve(__dirname, 'data', 'svg-with-script.svg'))
-            .pipe(svgFilter)
-            .on('data', function (chunk) {
-                chunks.push(chunk);
-            })
-            .on('end', function () {
-                var resultSvgText = Buffer.concat(chunks).toString('utf-8');
-                expect(resultSvgText, 'to match', /id="theId"/);
-                done();
-            })
-            .on('error', done);
+    it('should execute inline JavaScript with the specified id', function () {
+        return expect(
+            fs.createReadStream(Path.resolve(__dirname, 'data', 'svg-with-script.svg')),
+            'when piped through',
+            new SvgFilter({runScript: 'run', injectId: 'theId'}),
+            'to yield output satisfying',
+            'when decoded as', 'utf-8',
+            'to contain', 'id="theId"'
+        );
     });
 
-    it('should execute external JavaScript with the specified file name', function (done) {
-        var svgFilter = new SvgFilter({
+    it('should execute external JavaScript with the specified file name', function () {
+        return expect(
+            fs.createReadStream(Path.resolve(__dirname, 'data', 'dialog-information.svg')),
+            'when piped through',
+            new SvgFilter({
                 runScript: 'addBogusElement.js',
                 url: 'file://' + __dirname + '/data/',
                 bogusElementId: 'theBogusElementId'
             }),
-            chunks = [];
-        fs.createReadStream(Path.resolve(__dirname, 'data', 'dialog-information.svg'))
-            .pipe(svgFilter)
-            .on('data', function (chunk) {
-                chunks.push(chunk);
-            })
-            .on('end', function () {
-                var resultSvgText = Buffer.concat(chunks).toString('utf-8');
-                expect(resultSvgText, 'to match', /id="theBogusElementId"/);
-                done();
-            })
-            .on('error', done);
+            'to yield output satisfying',
+            'when decoded as', 'utf-8',
+            'to contain', 'id="theBogusElementId"'
+        );
     });
 
     it('should not emit data events while paused', function (done) {
